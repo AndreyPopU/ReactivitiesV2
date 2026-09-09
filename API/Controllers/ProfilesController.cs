e76a5b26e79e6855bcceeb9c75f0e680fd55d3d3
AndreyPopov <andreypopu@gmail.com>
2026-09-09 12:52:01 +0300

    End of Section 16

---
+++ API/Controllers/ProfilesController.cs
@@ -0,0 +1,40 @@
+using Application.Profiles.Commands;
+using Application.Profiles.DTOs;
+using Application.Profiles.Queries;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class ProfilesController : BaseApiController
+{
+    [HttpPost("add-photo")]
+    public async Task<ActionResult<Photo>> AddPhoto(IFormFile file)
+    {
+        return HandleResult(await Mediator.Send(new AddPhoto.Command { File = file }));
+    }
+
+    [HttpDelete("{photoId}/photos")]
+    public async Task<ActionResult> DeletePhoto(string photoId)
+    {
+        return HandleResult(await Mediator.Send(new DeletePhoto.Command { PhotoId = photoId }));
+    }
+
+    [HttpGet("{userId}/photos")]
+    public async Task<ActionResult<List<Photo>>> GetPhotosForUser(string userId)
+    {
+        return HandleResult(await Mediator.Send(new GetProfilePhotos.Query { UserId = userId }));
+    }
+
+    [HttpPut("{photoId}/set-main-photo")]
+    public async Task<ActionResult> SetMainPhoto(string photoId)
+    {
+        return HandleResult(await Mediator.Send(new SetMainPhoto.Command { PhotoId = photoId }));
+    }
+
+    [HttpGet("{userId}")]
+    public async Task<ActionResult<UserProfile>> GetProfile(string userId)
+    {
+        return HandleResult(await Mediator.Send(new GetProfile.Query { userId = userId }));
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Application.Interfaces;
 using Infrastructure.Security;
+using Infrastructure.Photos;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -34,6 +35,7 @@ builder.Services.AddMediatR(x =>
 });
 
 builder.Services.AddScoped<IUserAccessor, UserAccessor>();
+builder.Services.AddScoped<IPhotoService, PhotoService>();
 builder.Services.AddAutoMapper(cfg => { }, typeof(MappingProfiles).Assembly);
 builder.Services.AddValidatorsFromAssemblyContaining<CreateActivityValidator>();
 builder.Services.AddTransient<ExceptionMiddleware>(); // Add transient will only be instantiated when needed
@@ -53,6 +55,7 @@ builder.Services.AddAuthorization(opt =>
 });
 
 builder.Services.AddTransient<IAuthorizationHandler, IsHostRequirementHandler>();
+builder.Services.Configure<CloudinarySettings>(builder.Configuration.GetSection("CloudinarySettings"));
 
 var app = builder.Build();
 app.UseMiddleware<ExceptionMiddleware>();
